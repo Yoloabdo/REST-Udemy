ca56f3066c2d0c1e5e036ddbb678046fd4db78fa
yoloabdo <dhooom.da@gmail.com>
2016-04-05 21:51:26 +0200

    Tableview controller done

---
+++ REST apple/REST apple/MoviesTVCTableViewController.swift
@@ -0,0 +1,129 @@
+//
+//  MoviesTVCTableViewController.swift
+//  REST apple
+//
+//  Created by Abdulrhman  eaita on 4/5/16.
+//  Copyright © 2016 Abdulrhman  eaita. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTVCTableViewController: UITableViewController {
+
+    var videos = [Videos]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
+        
+        let api = APIManager()
+        
+        api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=50/explicit=true/json", completion: didLoadData)
+        
+
+    }
+    
+    func didLoadData(results: [Videos]) -> Void {
+        videos = results
+        tableView.reloadData()
+        
+    }
+    
+    func reachabilityStatusChanged() -> Void {
+        switch reachabilityStatus {
+        case WWAN:
+            self.view.backgroundColor = UIColor.yellowColor()
+        case WIFI:
+            view.backgroundColor = UIColor.greenColor()
+        case NOACCESS:
+            view.backgroundColor = UIColor.redColor()
+        default:
+            return
+        }
+    }
+    
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return videos.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = video._vname
+        cell.detailTextLabel?.text = video._vArtist
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
